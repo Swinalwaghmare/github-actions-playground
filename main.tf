47a3327f6a206fecf178c213c91bb1a832f5a687
Swinalwaghmare <swinalwaghmare28.com>
2025-11-24 15:20:19 +0530

    ami id added

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public-1" {
 }
 
 resource "aws_instance" "name" {
-    ami = ""
+    ami = "ami-0fa3fe0fa7920f68e"
     instance_type = "t2.micro"
     tags = {
       Name = "TF-EC2"
